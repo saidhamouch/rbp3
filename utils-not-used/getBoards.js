9fadc4e89066ddb79d583eeb88ea4ee69a5273ad
saidhamouch <said.hamouch01@gmail.com>
2025-11-22 15:56:29 +0100

    first upload 98

--- utils-not-used/getBoards.js
+++ utils-not-used/getBoards.js
@@ -1,25 +1,31 @@
 const axios = require("axios");
-require('dotenv').config();
+require("dotenv").config();
+const fs = require("fs");
 
-const token = process.env.PINTEREST_USER_TOKEN
+const token = "";
 const config = {
-  headers: { Authorization: `Bearer ${token}` }
+  headers: { Authorization: `Bearer ${token}` },
 };
 
 const getBoards = async () => {
-    try {
-      const boards = await axios.get(
-        'https://api.pinterest.com/v5/boards',
-        config
-      )
-        
-      console.log(boards.data.items);
-    //   fs.writeFile("boards.json", JSON.stringify((boards.data.items)), function (err) {
-    //     console.log(err);
-    //   });
-  
-      console.log(boards.data.items);
-    } catch (error) {
-      console.error(error)
-    }
+  try {
+    const boards = await axios.get(
+      "https://api.pinterest.com/v5/boards?page_size=250",
+      config
+    );
+
+    console.log(boards.data.items);
+    fs.writeFile(
+      "boards2.json",
+      JSON.stringify(boards.data.items),
+      function (err) {
+        console.log(err);
+      }
+    );
+
+    console.log(boards.data.items);
+  } catch (error) {
+    console.error(error);
   }
+};
+getBoards();
