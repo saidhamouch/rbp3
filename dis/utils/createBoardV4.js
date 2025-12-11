3133034eae4cf5f6a95543bc22cf046ac1cdd7ea
saidh007 <said.hamouch08@gmail.com>
2025-12-11 14:18:43 +0100

    mock3 1 start

--- dis/utils/createBoardV4.js
+++ dis/utils/createBoardV4.js
@@ -11,7 +11,7 @@ const createBoard = async (title) => {
   const editedTitle = editTitle(title);
   try {
     const payload = {
-      name: `${editedTitle} Displate Posters`,
+      name: `${editedTitle} Posters - Displate`,
       description: `${title} Displate Metal Posters`,
       privacy: "PUBLIC",
     };
@@ -41,3 +41,4 @@ module.exports = createBoard;
 // name: `${editedTitle} Metal Posters`,
 // name: `${editedTitle} Metal Posters - Displate`,
 // name: `${editedTitle} Displate Posters`,
+// name: `${editedTitle} Posters - Displate`,
